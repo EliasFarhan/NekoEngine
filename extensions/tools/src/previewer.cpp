e372dfcefbd55cde01de2b30ffd9f1277f65e84b
EliasFarhan <elias.farhan@gmail.com>
2019-08-08 15:49:21 +0200

    Adding setting to see the spine preview

--- extensions/tools/src/previewer.cpp
+++ extensions/tools/src/previewer.cpp
@@ -32,47 +32,52 @@ namespace editor
 
 void Previewer::Update(float dt)
 {
-    if(previewTexture_.getSize() == sf::Vector2u(0,0))
-    {
-        const auto imWindowSize = ImGui::GetWindowSize();
-        previewTexture_.create(unsigned(imWindowSize.x), unsigned(imWindowSize.y));
-    }
-    switch (status_)
-    {
-        case PreviewStatus::SpineAnimation:
-        {
-            previewTexture_.clear();
-            if(spineDrawable_.skeletonDrawable != nullptr)
-            {
-                spineDrawable_.SetPosition(sf::Vector2f(previewTexture_.getSize())/2.0f);
-                previewTexture_.draw(*spineDrawable_.skeletonDrawable);
-            }
-            previewTexture_.display();
-            ImGui::Image(previewTexture_.getTexture());
-            break;
-        }
-        default:
-            break;
-    }
+	if (previewTexture_.getSize() == sf::Vector2u(0, 0))
+	{
+		const auto imWindowSize = ImGui::GetContentRegionAvail();
+		previewTexture_.create(unsigned(imWindowSize.x), unsigned(imWindowSize.y));
+	}
+	switch (status_)
+	{
+	case PreviewStatus::SpineAnimation:
+	{
+		previewTexture_.clear();
+		if (spineDrawable_.skeletonDrawable != nullptr)
+		{
+			spineDrawable_.SetPosition(sf::Vector2f(previewTexture_.getSize()) / 2.0f);
+			previewTexture_.draw(*spineDrawable_.skeletonDrawable);
+			auto view = previewTexture_.getView();
+			const auto skeletonBound = spineDrawable_.skeletonDrawable->vertexArray->getBounds();
+			const sf::Vector2f ratio = sf::Vector2f(float(skeletonBound.width) * 2.0f / view.getSize().x, float(skeletonBound.height) * 2.0f / view.getSize().y);
+			view.setSize(view.getSize() * (ratio.x > ratio.y ? ratio.x : ratio.y));
+			previewTexture_.setView(view);
+		}
+		previewTexture_.display();
+		ImGui::Image(previewTexture_.getTexture());
+		break;
+	}
+	default:
+		break;
+	}
 }
 
 void Previewer::SetSpineAnimation(neko::BasicSpineDrawable* spineDrawablePtr)
 {
-    spineDrawable_.atlas = spineDrawablePtr->atlas;
-    spineDrawable_.skeletonData = spineDrawablePtr->skeletonData;
-    spineDrawable_.skeletonDrawable = std::make_shared<spine::SkeletonDrawable>(
-            spineDrawable_.skeletonData);
-    spineDrawable_.skeletonDrawable->update(0.0f);
+	spineDrawable_.atlas = spineDrawablePtr->atlas;
+	spineDrawable_.skeletonData = spineDrawablePtr->skeletonData;
+	spineDrawable_.skeletonDrawable = std::make_shared<spine::SkeletonDrawable>(
+		spineDrawable_.skeletonData);
+	spineDrawable_.skeletonDrawable->update(0.0f);
 
-    auto rect = spineDrawable_.skeletonDrawable->vertexArray->getBounds();
+	auto rect = spineDrawable_.skeletonDrawable->vertexArray->getBounds();
 
-    status_ = PreviewStatus::SpineAnimation;
+	status_ = PreviewStatus::SpineAnimation;
 
 }
 
 Previewer::~Previewer()
 {
-    spineDrawable_.atlas = nullptr;
-    spineDrawable_.skeletonData = nullptr;
+	spineDrawable_.atlas = nullptr;
+	spineDrawable_.skeletonData = nullptr;
 }
 }
