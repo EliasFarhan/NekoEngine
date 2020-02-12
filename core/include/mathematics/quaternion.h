e2d1ca291af7a8ab5ec14f51c864228715d55534
stephengrosjean.m@gmail.com <stephengrosjeam.m@gmail.com>
2020-02-12 12:26:01 +0100

    Added basic functions

--- core/include/mathematics/quaternion.h
+++ core/include/mathematics/quaternion.h
@@ -8,20 +8,50 @@ namespace neko
 	class Quaternion
 	{
 	public:
+		/*
+		The dot product between two rotations.
+		*/
 		float Dot(Quaternion a, Quaternion b)
 		{
 			return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
 		}
-		float Euler(float x, float y, float z)
-		{
 
-		}
+		/*
+		Returns the Inverse of rotation.
+		*/
 		Quaternion Inverse(Quaternion quaternion)
 		{
 
 		}
+
+		/*
+		Converts this quaternion to one with the same orientation but with a magnitude of 1.
+		*/
 		Quaternion Normalize(Quaternion quaternion)
 		{
+
+		}
+
+		/*
+		Creates a rotation which rotates angle degrees around axis.
+		*/
+		Vec3f AngleAxis(Quaternion quaternion, float angle, Vec3f axis) {
+
+		}
+
+		/*
+		Returns the angle in degrees between two rotations a and b.
+		*/
+		float Angle(Quaternion a, Quaternion b) {
+
+		}
+
+		/*
+		Returns a rotation that rotates z degrees around the z axis,
+		x degrees around the x axis, and y degrees around the y axis; 
+		applied in that order
+		*/
+		Quaternion Euler(Quaternion quaternion, Vec3f angle) {
 
 		}
 
