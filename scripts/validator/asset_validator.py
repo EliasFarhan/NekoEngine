ef3ae8c79044d054b1d09d217bcd82a171d3e60f
EliasFarhan <elias.farhan@gmail.com>
2020-03-08 19:28:44 +0100

    Correct bug with multithread compilation with python script

--- scripts/validator/asset_validator.py
+++ scripts/validator/asset_validator.py
@@ -14,11 +14,15 @@ def create_out_subdirectories(data_out):
         parent_dirs.append(data_path)
     for p in parent_dirs[::-1]:
         print(str(p.absolute()))
-        if not p.exists():
+        try:
             os.mkdir(str(p.absolute()))
+        except FileExistsError:
+            pass
+
 
 def define_asset_type(filename):
     pass
+
 
 if __name__ == "__main__":
     arguments = ", ".join(sys.argv)
