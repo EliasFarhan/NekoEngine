d0ba09e87c6dbaeba8fa2401fd9f9122bd0ddc87
Sebastien Feser <sedukafe@gmail.com>
2020-03-16 10:26:07 +0100

    created classes SmallFixedMap, DynamicMap, SmallDynamicMap

--- core/include/mathematics/map.h
+++ core/include/mathematics/map.h
@@ -70,147 +70,20 @@ private:
     // Allocator& allocator_;
 };
 
-
-
-/*template<typename Key, typename Value, size_t Capacity>
-class ArrayMap
+class DynamicMap
 {
-public:
-    // Constructor / destructor.
-    ArrayMap(const size_t sizeInBytes)
-    {
-        allocator = nullptr; // TODO: Pass this allocator to neko's container.
-        capacity = sizeInBytes / sizeof(Value);
-        pairs_ = pairs_[capacity];
-        for (auto& pair : pairs_)
-        {
-            pair = std::pair<xxh::hash_t<64>, Value>(0, Value());
-        }
-    }
+	
+};
 
-    inline bool IsEmpty()
-    {
-        return count_ == 0;
-    }
+class SmallFixedMap
+{
+	
+};
 
-    inline size_t Count()
-    {
-        return count_;
-    }
+class SmallDynamicMap
+{
+		
+};
 
-    inline size_t Capacity()
-    {
-        return capacity;
-    }
-
-    bool Add(const Key key, const Value value)
-    {
-        // TODO@Seb: check that key doesn't exist already in the map! Otherwise 1 key may have multiple values!
-
-        const xxh::hash_t<64> hash = xxh::xxhash<64>(&key, sizeof(Key));
-        const size_t len = capacity;
-        for (size_t i = 0; i < len; i++)
-        {
-            if (pairs_[i].first == 0)
-            {
-                pairs_[i].first = hash;
-                pairs_[i].second = value;
-                count_++;
-                return true;
-            }
-        }
-        // assert("Could not add to map!"); // TEMPORARY for testing purposes!
-        return false;
-    }
-
-    bool Remove(const Key key)
-    {
-        const xxh::hash_t<64> hash = xxh::xxhash<64>(&key, sizeof(Key));
-        const size_t len = capa;
-        for (int i = 0; i < len; i++)
-        {
-            if (pairs_[i].first == hash)
-            {
-                pairs_[i].first = 0;
-                pairs_[i].second = Value();
-                count_--;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void Clear()
-    {
-        for (auto& pair : pairs_)
-        {
-            pair = std::pair<xxh::hash_t<64>, Value>(0, Value());
-        }
-        count_ = 0;
-    }
-
-    bool Swap(const Key a, const Key b)
-    {
-        if (a == b || count_ < 2) return false;
-
-        const xxh::hash_t<64> hash0 = xxh::xxhash<64>(&a, sizeof(Key));
-        const xxh::hash_t<64> hash1 = xxh::xxhash<64>(&b, sizeof(Key));
-        const size_t len = count_;
-
-        size_t index0 = 0;
-        size_t index1 = 0;
-        size_t i = 0;
-        bool firstMatch = false;
-
-        do
-        {
-            auto hash = pairs_[i].first;
-            if (hash == hash0 || hash == hash1)
-            {
-                if (!firstMatch)
-                {
-                    index0 = i;
-                    firstMatch = true;
-                }
-                else
-                {
-                    index1 = i;
-                    goto RETURN;
-                }
-            }
-        } while (i++ < len);
-        return false; // Traversed whole map without finding both keys.
-
-        RETURN:
-        const auto temp = pairs_[index0].second;
-        pairs_[index0].second = pairs_[index1].second;
-        pairs_[index1].second = temp;
-        return true;
-    }
-
-    Value FindValue(const Key key) const
-    {
-        // TODO@Seb: Have to traverse whole vector regardless of count_ if there are holes in the memory layout!
-
-        const xxh::hash_t<64> hash = xxh::xxhash<64>(&key, sizeof(Key));
-        const size_t len = capacity; // TODO@Seb: this...       
-
-        for (size_t i = 0; i < len; i++)        //
-        {
-            if (pairs_[i].first == hash)
-            {
-                return pairs_[i].second;
-            }
-        }
-        // assert("Value not found!"); // TEMPORARY for testing purposes!
-        return 0;
-    }
-
-private:
-    std::array<std::pair<xxh::hash_t<64>, Value>, Capacity> pairs_;
-
-    //std::vector<std::pair<xxh::hash_t<64>, Value>> pairs_; // TODO: Replace this with neko's container when it's ready.
-    size_t count_ = 0;
-};*/
-
+	
 }// !neko
