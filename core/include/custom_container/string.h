22ad56fd66bacee9a637ab5c17e98810467449fa
Elias Farhan <elias.farhan@gmail.com>
2020-03-19 16:30:01 +0100

    Move string to custom container folder and add them

---
+++ core/include/custom_container/string.h
@@ -0,0 +1,62 @@
+#pragma once
+
+
+#include <string_view>
+#include <engine/custom_allocator.h>
+
+namespace neko
+{
+
+class String
+{
+    char* buffer_ = nullptr;
+    size_t length_ = 0;
+    FreeListAllocator& allocator_;
+
+public:
+    //-----------------------------------------------------------------------------
+    // Constructors
+    //-----------------------------------------------------------------------------
+
+    /// \brief Empty String Constructor
+    String(FreeListAllocator& allocator);
+
+    /// \brief char* String Constructor
+    String(FreeListAllocator& allocator, const std::string_view str);
+
+    /// \brief Default String Destructor
+    ~String();
+
+    /// \brief String length
+    size_t Length() const;
+
+    //TODO C_STR
+
+    //-----------------------------------------------------------------------------
+    // Operators
+    //-----------------------------------------------------------------------------
+
+    friend std::ostream& operator<< (std::ostream& so, const String& s);
+
+    char  operator[] (unsigned rhs) const;
+    char& operator[] (unsigned rhs);
+
+    String& operator= (const String& rhs);
+
+    String& operator+= (const String& rhs);
+
+
+    /*friend String operator+ (const String& lhs, const String& rhs);
+    friend String operator+ (const String& lhs, const char* rhs);
+    friend String operator+ (const char* lhs, const String& rhs);
+
+    friend bool operator== (const String& lhs, const String& rhs);
+    friend bool operator== (const String& lhs, const char* rhs);
+    friend bool operator== (const char* lhs, const String& rhs);*/
+
+    //TODO UTF-8
+private :
+
+};
+
+}
---
+++ core/src/custom_container/string.cpp
@@ -0,0 +1,166 @@
+/*
+ MIT License
+
+ Copyright (c) 2017 SAE Institute Switzerland AG
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <custom_container/string.h>
+
+namespace neko
+{
+    //-----------------------------------------------------------------------------
+    // Constructors
+    //-----------------------------------------------------------------------------
+    String::String(FreeListAllocator& allocator): allocator_(allocator)
+    {
+        length_ = 0;
+    }
+    
+    String::String(FreeListAllocator& allocator, std::string_view str): allocator_(allocator)
+    {
+        if (!str.empty())
+        {
+            buffer_ = static_cast<char*>(allocator_.Allocate(
+	            sizeof(char) * str.size(),
+	            alignof(char)));
+            length_ = str.size();
+        }
+
+    }
+
+    String::~String()
+    {
+    	if(buffer_ != nullptr)
+    	{
+            allocator_.Deallocate(buffer_);
+            length_ = 0;
+    	}
+    }
+
+    size_t String::Length() const
+    {
+        return length_;
+    }
+
+    //-----------------------------------------------------------------------------
+    // Operators
+    //-----------------------------------------------------------------------------
+    
+    std::ostream& operator<< (std::ostream& os, const String& s)
+    {
+        if (s.Length() > 0)
+        {
+            for (unsigned i = 0; i < s.Length(); i++) {
+                os << s[i];
+            }
+        }
+        else os << "";
+
+        return os;
+    }
+
+    char String::operator[] (unsigned rhs) const
+    {
+        if (rhs >= length_) throw 1;
+        return buffer_[rhs];
+    }
+
+    char& String::operator[] (unsigned rhs)
+    {
+        if (rhs >= length_) throw 1;
+        return buffer_[rhs];
+    }
+
+    String& String::operator= (const String& rhs)
+    {
+        if (this == &rhs) { return *this;}
+
+        delete buffer_;
+        length_ = rhs.Length();
+        buffer_ = new char[length_];
+
+        for (unsigned i = 0; i < length_; i++) {
+            buffer_[i] = rhs[i];
+        }
+
+        return *this;
+    }
+
+
+
+    String& String::operator+= (const String& rhs)
+    {
+        unsigned len = length_ + rhs.Length();
+        char* str = new char[len];
+
+        for (unsigned i = 0; i < length_; i++) {
+            str[i] = buffer_[i];
+        }
+
+        for (unsigned j = 0; j < rhs.Length(); j++) {
+            str[length_ + j] = rhs[j];
+        }
+
+        delete buffer_;
+        length_ = len;
+        buffer_ = str;
+        return *this;
+    }
+
+   /* String operator+ (const String& lhs, const String& rhs)
+    {
+   
+    }
+
+    String operator+ (const String& lhs, const char* rhs)
+    {
+    }
+
+    String operator+ (const char* lhs, const String& rhs)
+    {
+    }
+
+    bool operator== (const String& lhs, const String& rhs)
+    {
+        if (lhs.Length() != rhs.Length()) {
+            return false;
+        }
+
+        int cap = lhs.Length();
+        int n = 0;
+        while ((n < cap) && (lhs[n] == rhs[n])) {
+            n++;
+        }
+        return (n == cap);
+    }
+    //TODO STRNCOMP
+
+    bool operator== (const String& lhs, const char* rhs)
+    {
+        return (lhs == String(rhs));
+    }
+
+    bool operator== (const char* lhs, const String& rhs)
+    {
+        return (String(lhs) == rhs);
+    }*/
+
+}
