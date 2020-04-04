295de07b7a4773b89bc6604e2aac8cf4b200d154
Ethan <cavadiaethan@gmail.com>
2020-04-04 23:31:04 +0200

    Created circle_ncirle.h for the intersection optimization

--- core/include/mathematics/circle.h
+++ core/include/mathematics/circle.h
@@ -22,9 +22,6 @@
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
  */
-
-#include <vector>
-
 #include "mathematics/vector.h"
 #include "rect.h"
 
@@ -32,14 +29,12 @@ namespace neko
 {
     struct Circle
     {
-        explicit Circle(Vec2f Center, float Radius) : center(Center), radius(Radius)
+        Vec2f center;
+        const float radius;
+
+        explicit Circle(Vec2f Center, const float Radius) : center(Center), radius(Radius)
         {
 
-        }
-
-    	float GetRadius() const
-        {
-            return radius;
         }
 
         bool Intersects(Circle circle) const
@@ -47,92 +42,84 @@ namespace neko
             const float distanceX = center.x - circle.center.x;
             const float distanceY = center.y - circle.center.y;
             const float radiusSum = circle.radius + radius;
-        	
+
             return distanceX * distanceX + distanceY * distanceY <= radiusSum * radiusSum;
         }
-    	
-        bool IntersectsOther(Circle circle)
+
+        bool IntersectsOther(Circle circle) const
         {
             const Vec2f distanceVector = circle.center - center;
-            const float magnitude = Vec2f::Dot(distanceVector,distanceVector)*2;
+            const float magnitude = Vec2f::Dot(distanceVector, distanceVector) * 2;
 
             return magnitude <= circle.radius + radius;
         }
-    	
+
         bool RectCircleIntersects(Rect2f rect) const
         {
-			const Vec2f distanceVector = rect.center - center;
+            const Vec2f distanceVector = rect.center - center;
             const float magnitude = distanceVector.Magnitude();
-        	
-			if (magnitude <= rect.halfSize.x + radius)
-			{
-                return true;
-			}
 
-			if (magnitude <= rect.halfSize.y + radius)
-			{
+            if (magnitude <= rect.halfSize.x + radius)
+            {
                 return true;
-			}
-        	
+            }
+
+            if (magnitude <= rect.halfSize.y + radius)
+            {
+                return true;
+            }
+
             return false;
         }
-
-    	Vec2f center;
-        const float radius = 0;
     };
+
     struct Plan
     {
-        explicit Plan(Vec3f Pos, Vec3f Normal) : pos(Pos), normal(Normal)
-        {
-        }
-
         Vec3f pos;
         Vec3f normal;
+
+        explicit Plan(Vec3f Pos, Vec3f Normal) : pos(Pos), normal(Normal)
+        {
+
+        }
     };
 
-	struct Sphere
+    struct Sphere
     {
+        Vec3f center;
+        const float radius;
+    	
         explicit Sphere(Vec3f Center, float Radius) : center(Center), radius(Radius)
         {
 
         }
 
-        float GetRadius() const
-        {
-            return radius;
-        }
-		
         bool Intersects(Sphere sphere) const
         {
             const float distanceX = center.x - sphere.center.x;
             const float distanceY = center.y - sphere.center.y;
             const float distanceZ = center.z - sphere.center.z;
-        	
+
             const float radiusSum = sphere.radius + radius;
 
             return distanceX * distanceX + distanceY * distanceY + distanceZ * distanceZ <= radiusSum * radiusSum;
         }
-		
+
         bool IntersectsOther(Sphere sphere) const
         {
             const Vec3f distanceVector = sphere.center - center;
             const float magnitude = distanceVector.Magnitude();
             return magnitude <= sphere.radius + radius;
         }
-		
+
         bool IsPlanSphereIntersects(Plan plan) const
         {
             const float normMagnitude = plan.normal.Magnitude();
             const float dot = Vec3f::Dot(center - plan.pos, plan.normal);
-        	
+
             const float p = dot / normMagnitude;
 
-            return p < radius && p > -radius;
+            return p < radius&& p > -radius;
         }
-		
-        Vec3f center;
-        float radius;
     };
-    
-	
 }
--- test/test_circle.cpp
+++ test/test_circle.cpp
@@ -2,17 +2,39 @@
 #include <mathematics/rect.h>
 #include <gtest/gtest.h>
 
+/*TEST(Engine, TestCircle)
+{
+	neko::Circle circleA(neko::Vec2f(2, 2), 2);
+	neko::Circle circleB(neko::Vec2f(3, 3), 3);
+	neko::Circle circleC(neko::Vec2f(3, 3), 1);
+	neko::Circle circleD(neko::Vec2f(-2, 2), 2);
+
+	neko::Sphere sphere1(neko::Vec3f(2, 2, 2), 2);
+	neko::Sphere sphere2(neko::Vec3f(3, 3, 2), 3);
+	neko::Sphere sphere3(neko::Vec3f(3, 3, 2), 1);
+	neko::Sphere sphere4(neko::Vec3f(-2, 2, 2), 2);
+
+	neko::Rect2f rect(neko::Vec2f(1, 1), neko::Vec2f(2, 2));
+	neko::Plan plan(neko::Vec3f(0, 2, 0), neko::Vec3f(0, 1, 0));
+
+	EXPECT_TRUE(circleA.Intersects(circleB));
+	EXPECT_TRUE(circleB.Intersects(circleC));
+	EXPECT_TRUE(circleA.Intersects(circleD));
+	EXPECT_FALSE(circleC.Intersects(circleD));
+
+	EXPECT_TRUE(sphere1.Intersects(sphere2));
+
+	EXPECT_TRUE(circleA.RectCircleIntersects(rect));
+}*/
 TEST(Engine, TestCircle)
 {
 	const neko::Circle circleA(neko::Vec2f(2, 2), 2);
 	const neko::Circle circleB(neko::Vec2f(3, 3), 3);
 	const neko::Circle circleC(neko::Vec2f(3, 3), 1);
 	const neko::Circle circleD(neko::Vec2f(-2, 2), 2);
-	
-	const neko::Sphere sphere1(neko::Vec3f(2, 2,2), 2);
-	const neko::Sphere sphere2(neko::Vec3f(3, 3,2), 3);
-	const neko::Sphere sphere3(neko::Vec3f(3, 3,2), 1);
-	const neko::Sphere sphere4(neko::Vec3f(-2, 2,2), 2);
+
+	const neko::Sphere sphere1(neko::Vec3f(2, 2, 2), 2);
+	const neko::Sphere sphere2(neko::Vec3f(3, 3, 2), 2);
 
 	const neko::Rect2f rect(neko::Vec2f(1, 1), neko::Vec2f(2, 2));
 	const neko::Plan plan(neko::Vec3f(0, 2, 0), neko::Vec3f(0, 1, 0));
@@ -21,11 +43,23 @@ TEST(Engine, TestCircle)
 	EXPECT_TRUE(circleB.Intersects(circleC));
 	EXPECT_TRUE(circleA.Intersects(circleD));
 	EXPECT_FALSE(circleC.Intersects(circleD));
-	
+
 	EXPECT_TRUE(sphere1.Intersects(sphere2));
-	EXPECT_TRUE(sphere2.Intersects(sphere3));
-	EXPECT_TRUE(sphere1.Intersects(sphere4));
-	EXPECT_FALSE(sphere3.Intersects(sphere4));
-	
+
 	EXPECT_TRUE(circleA.RectCircleIntersects(rect));
- }
+	EXPECT_TRUE(sphere1.IsPlanSphereIntersects(plan));
+}
+/*TEST(Engine, TestNCircle)
+{	
+	std::array<neko::Sphere, 4> array;
+	array.fill(neko::Sphere(neko::Vec3f(3, 4,3), 4));
+
+	neko::NSphere n_circle(array);
+	auto result = n_circle.Intersect(array);
+
+	for (auto& tet : result) {
+		std::cout << tet << "\n";
+		EXPECT_TRUE(tet - array[0].Intersects(array[0]) < 0.01f);
+	}
+	
+}*/
