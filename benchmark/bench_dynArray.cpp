c37ad109672725260f2d7ef1f1f437604b1bb093
GJeannin0 <j6.guillaume@gmail.com>
2020-04-28 17:43:33 +0200

    benchmark dynarray

--- benchmark/bench_dynArray.cpp
+++ benchmark/bench_dynArray.cpp
@@ -11,6 +11,16 @@ const long toRange = 1 << 15;
 const float start = -100.0f;
 const float end = 100.0f;
 
+static void BM_Push(benchmark::State& state){
+	const size_t ALLOCATOR_HEADER_SIZE = 16;
+	const size_t SIZE = 131072;
+	void* start = malloc(SIZE + ALLOCATOR_HEADER_SIZE);
 
+	neko::FreeListAllocator allocator(SIZE + ALLOCATOR_HEADER_SIZE, start);
+	neko::DynArray<int> dyn(allocator);
 
-BENCHMARK_MAIN();
+	for (int i = 0; i < state.range(0); i++){
+		dyn.Push(i);
+	}
+}
+BENCHMARK(BM_Push)->Range(fromRange, toRange);
