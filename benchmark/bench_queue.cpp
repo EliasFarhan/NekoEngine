825ef8f7cc96acd00207035807e88a0e6155a975
AdamNaji <adam.naji@outlook.com>
2020-04-28 16:53:21 +0200

    benchmark for std::queue added

--- benchmark/bench_queue.cpp
+++ benchmark/bench_queue.cpp
@@ -1,5 +1,6 @@
 #include <utilities/custom_queue.h>
 #include <benchmark/benchmark.h>
+#include <queue>
 
 const unsigned long fromRange = 8;
 const unsigned long toRange = 1 << 15;
@@ -46,3 +47,23 @@ static void BM_QueueBasic(benchmark::State& state)
 }
 
 BENCHMARK(BM_QueueBasic)->Range(fromRange, toRange);
+static void BM_stdQueue(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		const auto length = state.range(0);
+		std::queue<int> queue;
+		for (int i = 0; i < length; i++)
+		{
+			queue.push(rand() % 32768);
+		}
+
+		for (int i = 0; i < length; i++)
+		{
+			queue.pop();
+		}
+
+	}
+}
+
+BENCHMARK(BM_stdQueue)->Range(fromRange, toRange);
