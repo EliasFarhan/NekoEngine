c368359c8cb7024e099d2c6a70432e3ca8377701
GJeannin0 <j6.guillaume@gmail.com>
2020-04-29 10:53:27 +0200

    fixed benchmark

--- benchmark/bench_dynArray.cpp
+++ benchmark/bench_dynArray.cpp
@@ -6,18 +6,24 @@
 const unsigned long fromRange = 8;
 const unsigned long toRange = 1 << 16;
 
-static void BM_Push(benchmark::State& state){
+static void BM_DynPush(benchmark::State& state)
+{
+    const size_t ALLOCATOR_HEADER_SIZE = 16;
+    const size_t SIZE = 4194304;
+    void* start = malloc(SIZE + ALLOCATOR_HEADER_SIZE);
 
-	const auto length = state.range(0);
-	const size_t ALLOCATOR_HEADER_SIZE = 16;
-	const size_t SIZE = 4194304;
-	void* start = malloc(SIZE + ALLOCATOR_HEADER_SIZE);
-
-	neko::FreeListAllocator allocator(SIZE + ALLOCATOR_HEADER_SIZE, start);
-	neko::DynArray<int> dyn(allocator);
-
-	for (int i = 0; i < length; i++){
-		dyn.Push(rand()%32768);
-	}
+    neko::FreeListAllocator allocator(SIZE + ALLOCATOR_HEADER_SIZE, start);
+	
+    const auto length = state.range(0);
+    neko::DynArray<int> dyn(allocator);
+	
+    for (int i = 0; i < length; i++){
+        dyn.Push(rand() % 32768);
+    }
+    for (auto _ : state){
+        for (size_t i = 0; i < dyn.GetSize(); i++) {
+            benchmark::DoNotOptimize(dyn);
+        }
+    }
 }
-BENCHMARK(BM_Push)->Range(fromRange, toRange);
+BENCHMARK(BM_DynPush)->Range(fromRange, toRange);
