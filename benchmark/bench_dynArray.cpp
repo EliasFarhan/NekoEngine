cef62df97fc821db0a9c5381acf79418ac94f5c0
GJeannin0 <j6.guillaume@gmail.com>
2020-04-29 11:02:34 +0200

    bench Vec compared to dyn

--- benchmark/bench_dynArray.cpp
+++ benchmark/bench_dynArray.cpp
@@ -1,6 +1,7 @@
 #include <benchmark/benchmark.h>
 #include <random>
 #include "engine/array.h"
+#include <vector>
 #include <mathematics/vector.h>
 
 const unsigned long fromRange = 8;
@@ -27,3 +28,19 @@ static void BM_DynPush(benchmark::State& state)
     }
 }
 BENCHMARK(BM_DynPush)->Range(fromRange, toRange);
+
+static void BM_VecPush(benchmark::State& state) {
+
+    const auto length = state.range(0);
+    std::vector<int> vec(state.range(0));
+
+    for (int i = 0; i < length; i++) {
+        vec.push_back(rand() % 32768);
+    }
+    for (auto _ : state) {
+        for (size_t i = 0; i < vec.size(); i++) {
+            benchmark::DoNotOptimize(vec);
+        }
+    }
+}
+BENCHMARK(BM_VecPush)->Range(fromRange, toRange);
