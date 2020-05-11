567ee2bd57a34f6a1aadec31eb5ac119c5f5d34f
Elias Farhan <elias.farhan@gmail.com>
2020-05-11 16:39:39 +0200

    Adding required basic math function

---
+++ core/include/mathematics/basic.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cmath>
+
+namespace neko
+{
+inline float Abs(float v)
+{
+	return std::abs(v);
+}
+inline bool Equal(float a, float b, float epsilon = 0.00001f)
+{
+	return Abs(a - b) < epsilon;
+}
+}
