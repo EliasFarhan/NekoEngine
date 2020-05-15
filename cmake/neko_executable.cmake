004a56b8dab42d2eb9a1c7cb5702ec67f5533cae
efarhan <elias.farhan@gmail.com>
2020-05-15 22:49:39 +0200

    Trying to make the WebGL2 work

--- cmake/neko_executable.cmake
+++ cmake/neko_executable.cmake
@@ -3,14 +3,14 @@ function(neko_bin_config binary)
     if(Emscripten)
         if("${CMAKE_BUILD_TYPE}" MATCHES "Debug")
             set_target_properties(${binary} PROPERTIES  COMPILE_FLAGS
-                " -s PTHREAD_POOL_SIZE=4 -s USE_PTHREADS=1 -s ALLOW_MEMORY_GROWTH=1  -g4 -O0 -fno-rtti -s ASSERTIONS=2 -s ALIASING_FUNCTION_POINTERS=1 -s SAFE_HEAP=1 -s DEMANGLE_SUPPORT=1  ")#-s SIMD=1
+                " -s USE_PTHREADS=1  -g4 -O0 -fno-rtti -s ASSERTIONS=2 -s ALIASING_FUNCTION_POINTERS=1 -s SAFE_HEAP=1 -s DEMANGLE_SUPPORT=1  ")#-s SIMD=1
         endif()
         if("${CMAKE_BUILD_TYPE}" MATCHES "Release")
         set_target_properties(${binary} PROPERTIES   COMPILE_FLAGS
-                " -s PTHREAD_POOL_SIZE=4  -s USE_PTHREADS=1 -O3 -fno-rtti -fno-exceptions -s ASSERTIONS=1 -s ALLOW_MEMORY_GROWTH=1 ")
+                " -s USE_PTHREADS=1 -O3 -fno-rtti -fno-exceptions -s ASSERTIONS=1 ")
         endif()
         set_target_properties(${binary} PROPERTIES  LINK_FLAGS
-                " --shared-memory -s ASSERTIONS=1 -s EXPORT_ALL=1 -s TOTAL_MEMORY=128MB --preload-file ${CMAKE_BINARY_DIR}/data@data/ ")
+                " -s PTHREAD_POOL_SIZE=4 -Wl,--shared-memory,--no-check-features -s ASSERTIONS=1 -s EXPORT_ALL=1 -s TOTAL_MEMORY=128MB --preload-file ${CMAKE_BINARY_DIR}/data@data/ ")
         set_target_properties(${binary} PROPERTIES SUFFIX ".html")
     elseif(MSVC)
             set_target_properties(${binary} PROPERTIES COMPILE_FLAGS " /arch:AVX2 /Oi ")
@@ -37,13 +37,15 @@ function(neko_lib_config library)
     if(Emscripten)
         if("${CMAKE_BUILD_TYPE}" MATCHES "Debug")
             set_target_properties(${library} PROPERTIES  COMPILE_FLAGS
-                " -s PTHREAD_POOL_SIZE=4 -s USE_PTHREADS=1 -s ALLOW_MEMORY_GROWTH=1 -g4 -O0 -fno-rtti -s ASSERTIONS=2 -s ALIASING_FUNCTION_POINTERS=1 -s SAFE_HEAP=1 -s DEMANGLE_SUPPORT=1  ") #-s SIMD=1
+                " -s USE_PTHREADS=1  -g4 -O0 -fno-rtti -s ASSERTIONS=2 -s ALIASING_FUNCTION_POINTERS=1 -s SAFE_HEAP=1 -s DEMANGLE_SUPPORT=1  ") #-s SIMD=1
         endif()
         if("${CMAKE_BUILD_TYPE}" MATCHES "Release")
             set_target_properties(${library} PROPERTIES  COMPILE_FLAGS
-                " -s PTHREAD_POOL_SIZE=4 -s USE_PTHREADS=1 -s ALLOW_MEMORY_GROWTH=1 -O3 -fno-rtti -fno-exceptions -s ASSERTIONS=1 ")
+                " -s USE_PTHREADS=1 -O3 -fno-rtti -fno-exceptions -s ASSERTIONS=1 ")
         endif()
-        set_property(TARGET ${library} APPEND_STRING PROPERTY LINK_FLAGS " -s EXPORT_ALL=1 -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=1 ")
+        set_property(TARGET ${library} APPEND_STRING PROPERTY LINK_FLAGS
+                " -s PTHREAD_POOL_SIZE=4  -s EXPORT_ALL=1 -s TOTAL_MEMORY=128MB -s ASSERTIONS=1 ")
+        #-Wl,--shared-memory,--no-check-features
     elseif(MSVC)
         set_target_properties(${library} PROPERTIES COMPILE_FLAGS " /arch:AVX2 /Oi ")
     elseif(UNIX)
