1c9286d84f1f84b88064d7e5b98eb504e776646c
Elias Farhan <elias.farhan@gmail.com>
2020-06-09 10:07:46 +0200

    Starting to work on the HDR sample

--- main/comp_graph/include/19_hello_hdr/hdr_program.h
+++ main/comp_graph/include/19_hello_hdr/hdr_program.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include "comp_graph/sample_program.h"
+#include "gl/shader.h"
+#include "gl/shape.h"
+#include "gl/texture.h"
+
 namespace neko
 {
 class HelloHdrProgram : public SampleProgram
 {
+    struct Light
+    {
+        Vec3f lightPos_;
+        Color3 lightColor_;
+    };
 public:
-    void OnEvent(const SDL_Event& event) override;
+
 
     void Init() override;
 
@@ -17,6 +26,33 @@ public:
     void DrawImGui() override;
 
     void Render() override;
+    void OnEvent(const SDL_Event& event) override;
+private:
+    enum HdrFlags : std::uint8_t
+    {
+        NONE = 0u,
+        ENABLE_HDR = 1u<<0u
+    };
+    unsigned int hdrFbo_ = 0;
+    unsigned int hdrColorBuffer_ = 0;
+    unsigned int hdrRbo_ = 0;
+    gl::Shader hdrShader_;
+    gl::RenderQuad hdrPlane_{Vec3f::zero, Vec2f::one*2.0f};
+
+    std::array<Light, 4> lights_=std::array<Light,4>{{
+            {Vec3f( 0.0f,  0.0f, 49.5f),Color3(200.0f, 200.0f, 200.0f)},
+            {Vec3f(-1.4f, -1.9f, 9.0f), Color3(0.1f, 0.0f, 0.0f)},
+            {Vec3f( 0.0f, -1.8f, 4.0f), Color3(0.0f, 0.0f, 0.2f)},
+            {Vec3f( 0.8f, -1.7f, 6.0f), Color3(0.0f, 0.1f, 0.0f)}
+    }};
+
+    gl::RenderCuboid cube_{Vec3f::zero, Vec3f(1.0f,1.0f,10.0f)};
+    gl::Texture cubeTexture_;
+    gl::Shader cubeShader_;
+
+    sdl::Camera3D camera_;
+    float exposure_ = 1.0f;
+    std::uint8_t flags_ = NONE;
 
 };
 }
--- main/comp_graph/src/19_hello_hdr/hdr_program.cpp
+++ main/comp_graph/src/19_hello_hdr/hdr_program.cpp
@@ -1,35 +1,117 @@
 #include "19_hello_hdr/hdr_program.h"
+#include "imgui.h"
 
 namespace neko
 {
 
-void HelloHdrProgram::OnEvent(const SDL_Event& event)
-{
-
-}
 
 void HelloHdrProgram::Init()
 {
+    const auto& config = BasicEngine::GetInstance()->config;
+    cube_.Init();
+    cubeShader_.LoadFromFile(
+            config.dataRootPath + "shaders/19_hello_hdr/tunnel.vert",
+            config.dataRootPath + "shaders/19_hello_hdr/tunnel.frag");
+    cubeTexture_.SetTextureFlags(gl::Texture::TextureFlags(gl::Texture::MIRROR_REPEAT_WRAP | gl::Texture::DEFAULT));
+    cubeTexture_.SetPath(config.dataRootPath+"sprites/brickwall/brickwall.jpg");
+    cubeTexture_.LoadFromDisk();
+
+    glGenFramebuffers(1, &hdrFbo_);
+    // create floating point color buffer
+    glGenTextures(1, &hdrColorBuffer_);
+    glBindTexture(GL_TEXTURE_2D, hdrColorBuffer_);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA16F, config.windowSize.x, config.windowSize.y, 0, GL_RGBA, GL_FLOAT, NULL);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // create depth buffer (renderbuffer)
+    glGenRenderbuffers(1, &hdrRbo_);
+    glBindRenderbuffer(GL_RENDERBUFFER, hdrRbo_);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, config.windowSize.x, config.windowSize.y);
+    // attach buffers
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFbo_);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, hdrColorBuffer_, 0);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, hdrRbo_);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    {
+        logDebug("[Error] Framebuffer not complete!");
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    hdrPlane_.Init();
 
 }
 
 void HelloHdrProgram::Update(seconds dt)
 {
-
+    std::lock_guard<std::mutex> lock(updateMutex_);
+    const auto& config = BasicEngine::GetInstance()->config;
+    camera_.SetAspect(config.windowSize.x, config.windowSize.y);
+    camera_.Update(dt);
 }
 
 void HelloHdrProgram::Destroy()
 {
+    cubeTexture_.Destroy();
+    cubeShader_.Destroy();
+    cube_.Destroy();
 
+    glDeleteFramebuffers(1, &hdrFbo_);
+    glDeleteTextures(1, &hdrColorBuffer_);
+    glDeleteRenderbuffers(1, &hdrRbo_);
 }
 
 void HelloHdrProgram::DrawImGui()
 {
-
+    ImGui::Begin("Hdr Program");
+    bool enableHdr = flags_ & ENABLE_HDR;
+    if(ImGui::Checkbox("Enable Hdr", &enableHdr))
+    {
+        flags_ = enableHdr ? flags_ | ENABLE_HDR : flags_ & ~ENABLE_HDR;
+    }
+    ImGui::SliderFloat("Exposure", &exposure_, 0.0f, 3.0f);
+    ImGui::End();
 }
 
 void HelloHdrProgram::Render()
 {
+    if (!cubeTexture_.IsLoaded())
+    {
+        return;
+    }
+    std::lock_guard<std::mutex> lock(updateMutex_);
 
+    glBindFramebuffer(GL_FRAMEBUFFER, hdrFbo_);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    cubeShader_.Bind();
+    cubeShader_.SetMat4("view", camera_.GenerateViewMatrix());
+    cubeShader_.SetMat4("projection", camera_.GenerateProjectionMatrix());
+    cubeShader_.SetMat4("model", Mat4f::Identity);
+    cubeShader_.SetMat4("transposeInverseModel", Mat4f::Identity);
+    cubeShader_.SetInt("diffuseTexture", 0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, cubeTexture_.GetTextureId());
+    for(size_t i = 0; i < lights_.size(); i++)
+    {
+        cubeShader_.SetVec3("lights["+std::to_string(i)+"].Position", lights_[i].lightPos_);
+        cubeShader_.SetVec3("lights["+std::to_string(i)+"].Color", lights_[i].lightColor_);
+    }
+    cubeShader_.SetInt("lightNmb", lights_.size());
+    cubeShader_.SetBool("inverseNormals", true);
+    cube_.Draw();
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    hdrShader_.Bind();
+    hdrShader_.SetInt("hdrBuffer", 0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, hdrColorBuffer_);
+    hdrShader_.SetBool("hdr", flags_&ENABLE_HDR);
+    hdrShader_.SetFloat("exposure", exposure_);
+    hdrPlane_.Draw();
+
+}
+
+void HelloHdrProgram::OnEvent(const SDL_Event& event)
+{
+    camera_.OnEvent(event);
 }
 }
